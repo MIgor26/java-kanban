507b63a920d3fb2ef195a14f18924add8747bd99
Igor Mikhaylov <pomivan1@yandex.ru>
2025-04-02 07:31:02 +0300

    Техническое задание 7. Итеррация №3. Проверка ГитХаб

--- src/exception/ManagerSaveException.java
+++ src/exception/ManagerSaveException.java
@@ -3,7 +3,7 @@ package exception;
 import java.io.IOException;
 
 public class ManagerSaveException extends RuntimeException {
-    public ManagerSaveException (String message, IOException cause) {
+    public ManagerSaveException(String message, IOException cause) {
         super(message, cause);
     }
 }
